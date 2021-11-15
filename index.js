0d4427114b7cdb111e69f84057c47ca768707164
Link1515 <h26281660@gmail.com>
2021-11-15 21:04:22 +0800

    返回五個最近的地點

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 import linebot from 'linebot'
 import axios from 'axios'
-import fs from 'fs'
+// import fs from 'fs'
 
 import { distance } from './distance.js'
 
@@ -15,21 +15,43 @@ const url =
   'https://iplay.sa.gov.tw/api/GymSearchAllList?$format=application/json;odata.metadata=none&Keyword=排球場&City=新北市&GymType=排球場'
 
 bot.on('message', async (event) => {
-  if (event.message.latitude) {
-    const myLatitude = event.message.latitude
-    const myLongitude = event.message.longitude
-    const { data } = await axios.get(encodeURI(url))
+  try {
+    if (event.message.latitude) {
+      const myLatitude = event.message.latitude
+      const myLongitude = event.message.longitude
+      const { data } = await axios.get(encodeURI(url))
 
-    let minDis = 99999999
+      const minDistanceData = []
+      const replies = []
 
-    for (let i = 0; i < data.length; i++) {
-      const location = data[i].LatLng.split(',')
-      if (distance(myLatitude, myLongitude, location[0], location[1], 'K') < minDis) {
-        minDis = distance(myLatitude, myLongitude, location[0], location[1], 'K')
+      for (let i = 0; i < data.length; i++) {
+        const location = data[i].LatLng.split(',')
+
+        if (i < 5) {
+          minDistanceData.push({ index: i, distance: distance(myLatitude, myLongitude, location[0], location[1], 'K') })
+          minDistanceData.sort((a, b) => a.distance - b.distance)
+        } else {
+          if (distance(myLatitude, myLongitude, location[0], location[1], 'K') < minDistanceData[4].distance) {
+            minDistanceData.pop()
+            minDistanceData.push({
+              index: i,
+              distance: distance(myLatitude, myLongitude, location[0], location[1], 'K')
+            })
+            minDistanceData.sort((a, b) => a.distance - b.distance)
+          }
+        }
       }
-    }
 
-    console.log(minDis)
+      for (let i = 0; i < minDistanceData.length; i++) {
+        replies.push(data[minDistanceData[i].index].Name)
+      }
+
+      // console.log(minDistanceData)
+      console.log(replies)
+      event.reply(replies)
+    }
+  } catch (error) {
+    console.log(error)
   }
 
   // const meg = event
