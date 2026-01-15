fd5709a8dccceba1f0889b8b006146ea0b790af1
Link1515 <terrylin1515@gmail.com>
2026-01-15 17:07:57 +0800

    chore: localize flex message alt text

--- template/createFlexPlaces.ts
+++ template/createFlexPlaces.ts
@@ -5,7 +5,7 @@ import { createPlacesBubbles } from './createPlacesBubbles'
 export const createFlexPlaces = (placeInfoList: Required<PlaceInfo>[]) => {
   const flexPlaces: FlexPlaces = {
     type: 'flex',
-    altText: '5 closest volleyball court',
+    altText: '5 個最近的排球場',
     contents: {
       type: 'carousel',
       contents: createPlacesBubbles(placeInfoList)
