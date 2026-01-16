64bf6ddf563d533a1a3684e66090a837f127bd8f
Link1515 <terrylin1515@gmail.com>
2026-01-16 15:50:04 +0800

    fix: return reply text promise from message helpers

--- handlers/textHandler/functions/errorMsg.ts
+++ handlers/textHandler/functions/errorMsg.ts
@@ -2,5 +2,5 @@ import { replyText } from '@utils/index'
 import messages from '@data/messages.json'
 
 export const errorMsg = (replyToken: string) => {
-  replyText(replyToken, messages.tryAgain)
+  return replyText(replyToken, messages.tryAgain)
 }
--- handlers/textHandler/functions/tutorialMsg.ts
+++ handlers/textHandler/functions/tutorialMsg.ts
@@ -2,5 +2,5 @@ import { replyText } from '@utils/index'
 import messages from '@data/messages.json'
 
 export const tutorialMsg = (replyToken: string) => {
-  replyText(replyToken, messages.tutorial)
+  return replyText(replyToken, messages.tutorial)
 }
