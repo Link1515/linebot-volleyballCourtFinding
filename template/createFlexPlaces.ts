3421d411890d50568056f44c0a9e50172accbc0c
Link1515 <terrylin1515@gmail.com>
2026-01-16 09:14:49 +0800

    fix: update flex alt text

--- template/createFlexPlaces.ts
+++ template/createFlexPlaces.ts
@@ -5,7 +5,7 @@ import { createPlacesBubbles } from '@template/createPlacesBubbles'
 export const createFlexPlaces = (placeInfoList: Required<PlaceInfo>[]) => {
   const flexPlaces: FlexPlaces = {
     type: 'flex',
-    altText: '5 個最近的排球場',
+    altText: '距離您最近的球場',
     contents: {
       type: 'carousel',
       contents: createPlacesBubbles(placeInfoList)
