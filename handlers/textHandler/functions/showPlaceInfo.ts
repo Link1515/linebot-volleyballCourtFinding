53e673fbfe8b065bc2c2704200fab165cfd486bd
Link1515 <terrylin1515@gmail.com>
2026-01-16 15:46:46 +0800

    refactor: use parseLatLng helper

--- handlers/textHandler/functions/showPlaceInfo.ts
+++ handlers/textHandler/functions/showPlaceInfo.ts
@@ -4,25 +4,18 @@ import type { PlaceInfo } from '@data/types'
 import rawPlaceInfoList from '@data/placeInfoList.json'
 import messages from '@data/messages.json'
 import { getWeatherInfo } from '@api/getWeatherInfo'
-import { replyText } from '@projectRoot/utils'
+import { parseLatLng, replyText } from '@projectRoot/utils'
 
 const placeInfoList = rawPlaceInfoList as PlaceInfo[]
 
 export const showPlaceInfo = async (replyToken: string, message: webhook.TextMessageContent) => {
   const title = message.text.replace('go ', '')
 
-  // query place info
-  let city: string
-  let address: string
-  let latitude: number
-  let longitude: number
-
   for (const placeInfo of placeInfoList) {
     if (placeInfo.Name === title) {
-      address = placeInfo.Address
-      city = placeInfo.Address.slice(0, 3)
-      latitude = +placeInfo.LatLng.split(',')[0]
-      longitude = +placeInfo.LatLng.split(',')[1]
+      const address = placeInfo.Address
+      const city = placeInfo.Address.slice(0, 3)
+      const { latitude, longitude } = parseLatLng(placeInfo.LatLng)
 
       const weatherStr = (await getWeatherInfo(city)) as string
 
